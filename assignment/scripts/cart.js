364966f47b1fa84e80d37c628a21f76fcbf5f258
SAMCFADDEN <99680930+SAMCFADDEN@users.noreply.github.com>
2023-04-12 20:58:28 -0400

    Update cart.js

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,29 @@
-console.log('***** Cart Functions *****');
+console.log("***** Cart Functions *****");
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item) {
+  basket.push(item);
+  return true;
+}
+
+addItem("pizza");
+console.log(addItem("steak"));
+console.log(basket);
+
+function listItems(thisBasket) {
+  for (food of thisBasket) {
+    console.log(food);
+  }
+}
+
+listItems(basket);
+
+function empty() {
+  basket = [];
+}
+
+empty();
+console.log(basket);
